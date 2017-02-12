600b3e2e83f592cf2479d7a773e65a44e7e3f98e
libbyschuknight <libbysk@gmail.com>
2017-02-12 14:00:50 +1300

    Add new spec @ 19.27

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117190328) do
+ActiveRecord::Schema.define(version: 20170212001717) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
   end
 
 end
