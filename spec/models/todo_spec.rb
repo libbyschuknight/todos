1375b3d0690c4dc65af813b574ba71690795272b
Libby Schumacher-Knight <libbysk@gmail.com>
2017-02-24 08:32:34 +1300

    Refactor logic in completions controller into model

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,13 +1,24 @@
 require "rails_helper"
 
-RSpec.describe Todo, "#completed?" do
-  it "returns true if completed_at is set" do
-    todo = Todo.new(completed_at: Time.current)
-    expect(todo).to be_completed
+RSpec.describe Todo do
+  describe "#completed?" do
+    it "returns true if completed_at is set" do
+      todo = Todo.new(completed_at: Time.current)
+      expect(todo).to be_completed
+    end
+
+    it "returns false if completed_at is nil" do
+      todo = Todo.new(completed_at: nil)
+      expect(todo).not_to be_completed
+    end
   end
 
-  it "returns false if completed_at is nil" do
-    todo = Todo.new(completed_at: nil)
-    expect(todo).not_to be_completed
+  describe "#complete! " do
+    it "update completed_at" do
+      todo = Todo.create!(completed_at: nil)
+      todo.complete!
+      todo.reload
+      expect(todo).to be_completed
+    end
   end
 end
